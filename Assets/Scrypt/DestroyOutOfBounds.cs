515fba27a5005f6dd1c8742afeb2cc09f4b3663e
itroy32tq <139143911+itroy32tq@users.noreply.github.com>
2023-09-13 16:30:14 +0300

    make helthbar
    сделал хелсбар, стандартным способом. Все префабы животных с очень разным скейлингом. Всех надо тюнить отдельно

--- Assets/Scrypt/DestroyOutOfBounds.cs
+++ Assets/Scrypt/DestroyOutOfBounds.cs
@@ -6,6 +6,8 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float _topBound = 30;
     private float _lowerBound = -10;
+    public float speed;
+    public string playerName;
 
     // Update is called once per frame
     void Update()
@@ -19,5 +21,6 @@ public class DestroyOutOfBounds : MonoBehaviour
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+
     }
 }
--- Assets/Scrypt/DetectCollisions.cs
+++ Assets/Scrypt/DetectCollisions.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DetectCollisions : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField, Range(0f, 6f)] int _hits = 2;
+    [SerializeField] private Image healthBar;
+    private float fill;
+    private float fill_max;
+    
+    private void Start()
     {
-        Destroy(gameObject);
+        
+        fill_max = _hits;
+
+        healthBar = GetComponentInChildren<Image>();
+        healthBar.fillAmount = 1f;
+    }
+    private void OnTriggerEnter(Collider other)
+    { 
+         _hits--;
+        fill = _hits / fill_max;
+        healthBar.fillAmount = fill;
+
+        if (_hits < 1)
+        {
+            
+            Destroy(gameObject);
+
+        } 
+
         Destroy(other.gameObject);
     }
 }
